0a1856d587ad0503613d00e1b04da129c06d1995
Ruben Smit <RubenSmit@users.noreply.github.com>
2020-12-27 15:11:43 +0100

    Adapting code for hid angle sensor

--- nrf/src/main.cpp
+++ nrf/src/main.cpp
@@ -1,19 +1,3 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2017 ARM Limited
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
- 
 #include <stdio.h>
  
 #include "platform/Callback.h"
@@ -29,45 +13,32 @@
 using mbed::callback;
  
 /**
- * A Clock service that demonstrate the GattServer features.
- *
- * The clock service host three characteristics that model the current hour,
- * minute and second of the clock. The value of the second characteristic is
- * incremented automatically by the system.
- *
- * A client can subscribe to updates of the clock characteristics and get
- * notified when one of the value is changed. Clients can also change value of
- * the second, minute and hour characteristric.
+ * BLE Human Interface Device service with a Angle Characterisic returning 
+ * the angle of a potentiometer connected to analog input 0.
  */
-class ClockService {
-    typedef ClockService Self;
+class HidService {
+    typedef HidService Self;
  
 public:
-    ClockService() :
-        _hour_char("485f4145-52b9-4644-af1f-7a6b9322490f", 0),
-        _minute_char("0a924ca7-87cd-4699-a3bd-abdcd9cf126a", 0),
-        _second_char("8dd6a1b7-bc75-4741-8a26-264af75807de", 0),
-        _clock_service(
-            /* uuid */ "51311102-030e-485f-b122-f8f381aa84ed",
-            /* characteristics */ _clock_characteristics,
-            /* numCharacteristics */ sizeof(_clock_characteristics) /
-                                     sizeof(_clock_characteristics[0])
+    HidService() :
+        _angle_char(0x2763, 0),
+        _hid_service(
+            /* uuid */ 0x1812,
+            /* characteristics */ _hid_characteristics,
+            /* numCharacteristics */ sizeof(_hid_characteristics) /
+                                     sizeof(_hid_characteristics[0])
         ),
         _server(NULL),
-        _event_queue(NULL)
+        _event_queue(NULL),
+        _angle_sensor(A0),
+        _heartbeat_led(LED1)
     {
         // update internal pointers (value, descriptors and characteristics array)
-        _clock_characteristics[0] = &_hour_char;
-        _clock_characteristics[1] = &_minute_char;
-        _clock_characteristics[2] = &_second_char;
+        _hid_characteristics[0] = &_angle_char;
  
         // setup authorization handlers
-        _hour_char.setWriteAuthorizationCallback(this, &Self::authorize_client_write);
-        _minute_char.setWriteAuthorizationCallback(this, &Self::authorize_client_write);
-        _second_char.setWriteAuthorizationCallback(this, &Self::authorize_client_write);
+        // _angle_char.setWriteAuthorizationCallback(this, &Self::authorize_client_write);
     }
- 
- 
  
     void start(BLE &ble_interface, events::EventQueue &event_queue)
     {
@@ -79,32 +50,29 @@ public:
         _event_queue = &event_queue;
  
         // register the service
-        printf("Adding demo service\r\n");
-        ble_error_t err = _server->addService(_clock_service);
+        printf("Adding service\r\n");
+        ble_error_t err = _server->addService(_hid_service);
  
         if (err) {
-            printf("Error %u during demo service registration.\r\n", err);
+            printf("Error %u during service registration.\r\n", err);
             return;
         }
  
         // read write handler
         _server->onDataSent(as_cb(&Self::when_data_sent));
-        _server->onDataWritten(as_cb(&Self::when_data_written));
         _server->onDataRead(as_cb(&Self::when_data_read));
  
         // updates subscribtion handlers
         _server->onUpdatesEnabled(as_cb(&Self::when_update_enabled));
         _server->onUpdatesDisabled(as_cb(&Self::when_update_disabled));
-        _server->onConfirmationReceived(as_cb(&Self::when_confirmation_received));
  
         // print the handles
-        printf("clock service registered\r\n");
-        printf("service handle: %u\r\n", _clock_service.getHandle());
-        printf("\thour characteristic value handle %u\r\n", _hour_char.getValueHandle());
-        printf("\tminute characteristic value handle %u\r\n", _minute_char.getValueHandle());
-        printf("\tsecond characteristic value handle %u\r\n", _second_char.getValueHandle());
+        printf("human interface device service registered\r\n");
+        printf("service handle: %u\r\n", _hid_service.getHandle());
+        printf("\angle characteristic value handle %u\r\n", _angle_char.getValueHandle());
  
-        _event_queue->call_every(1000 /* ms */, callback(this, &Self::increment_second));
+        _event_queue->call_every(1000 /* ms */, callback(this, &Self::read_angle));
+        _event_queue->call_every(1000 /* ms */, callback(this, &Self::blink_led));
     }
  
 private:
@@ -118,35 +86,6 @@ private:
     }
  
     /**
-     * Handler called after an attribute has been written.
-     */
-    void when_data_written(const GattWriteCallbackParams *e)
-    {
-        printf("data written:\r\n");
-        printf("\tconnection handle: %u\r\n", e->connHandle);
-        printf("\tattribute handle: %u", e->handle);
-        if (e->handle == _hour_char.getValueHandle()) {
-            printf(" (hour characteristic)\r\n");
-        } else if (e->handle == _minute_char.getValueHandle()) {
-            printf(" (minute characteristic)\r\n");
-        } else if (e->handle == _second_char.getValueHandle()) {
-            printf(" (second characteristic)\r\n");
-        } else {
-            printf("\r\n");
-        }
-        printf("\twrite operation: %u\r\n", e->writeOp);
-        printf("\toffset: %u\r\n", e->offset);
-        printf("\tlength: %u\r\n", e->len);
-        printf("\t data: ");
- 
-        for (size_t i = 0; i < e->len; ++i) {
-            printf("%02X", e->data[i]);
-        }
- 
-        printf("\r\n");
-    }
- 
-    /**
      * Handler called after an attribute has been read.
      */
     void when_data_read(const GattReadCallbackParams *e)
@@ -154,12 +93,8 @@ private:
         printf("data read:\r\n");
         printf("\tconnection handle: %u\r\n", e->connHandle);
         printf("\tattribute handle: %u", e->handle);
-        if (e->handle == _hour_char.getValueHandle()) {
-            printf(" (hour characteristic)\r\n");
-        } else if (e->handle == _minute_char.getValueHandle()) {
-            printf(" (minute characteristic)\r\n");
-        } else if (e->handle == _second_char.getValueHandle()) {
-            printf(" (second characteristic)\r\n");
+        if (e->handle == _angle_char.getValueHandle()) {
+            printf(" (angle characteristic)\r\n");
         } else {
             printf("\r\n");
         }
@@ -187,117 +122,33 @@ private:
     }
  
     /**
-     * Handler called when an indication confirmation has been received.
-     *
-     * @param handle Handle of the characteristic value that has emitted the
-     * indication.
-     */
-    void when_confirmation_received(GattAttribute::Handle_t handle)
-    {
-        printf("confirmation received on handle %d\r\n", handle);
-    }
- 
-    /**
-     * Handler called when a write request is received.
-     *
-     * This handler verify that the value submitted by the client is valid before
-     * authorizing the operation.
-     */
-    void authorize_client_write(GattWriteAuthCallbackParams *e)
-    {
-        printf("characteristic %u write authorization\r\n", e->handle);
- 
-        if (e->offset != 0) {
-            printf("Error invalid offset\r\n");
-            e->authorizationReply = AUTH_CALLBACK_REPLY_ATTERR_INVALID_OFFSET;
-            return;
-        }
- 
-        if (e->len != 1) {
-            printf("Error invalid len\r\n");
-            e->authorizationReply = AUTH_CALLBACK_REPLY_ATTERR_INVALID_ATT_VAL_LENGTH;
-            return;
-        }
- 
-        if ((e->data[0] >= 60) ||
-            ((e->data[0] >= 24) && (e->handle == _hour_char.getValueHandle()))) {
-            printf("Error invalid data\r\n");
-            e->authorizationReply = AUTH_CALLBACK_REPLY_ATTERR_WRITE_NOT_PERMITTED;
-            return;
-        }
- 
-        e->authorizationReply = AUTH_CALLBACK_REPLY_SUCCESS;
-    }
- 
-    /**
      * Increment the second counter.
      */
-    void increment_second(void)
+    void read_angle(void)
     {
-        uint8_t second = 0;
-        ble_error_t err = _second_char.get(*_server, second);
+        uint8_t angle = (uint8_t) map(_angle_sensor.read(), 0, 1, 0, 360);
+ 
+        ble_error_t err = _angle_char.set(*_server, angle);
         if (err) {
-            printf("read of the second value returned error %u\r\n", err);
+            printf("write of the angle value returned error %u\r\n", err);
             return;
-        }
- 
-        second = (second + 1) % 60;
- 
-        err = _second_char.set(*_server, second);
-        if (err) {
-            printf("write of the second value returned error %u\r\n", err);
-            return;
-        }
- 
-        if (second == 0) {
-            increment_minute();
         }
     }
- 
+
     /**
-     * Increment the minute counter.
+     * Map a value in a range to another range
      */
-    void increment_minute(void)
+    float map(float x, float in_min, float in_max, float out_min, float out_max)
     {
-        uint8_t minute = 0;
-        ble_error_t err = _minute_char.get(*_server, minute);
-        if (err) {
-            printf("read of the minute value returned error %u\r\n", err);
-            return;
-        }
- 
-        minute = (minute + 1) % 60;
- 
-        err = _minute_char.set(*_server, minute);
-        if (err) {
-            printf("write of the minute value returned error %u\r\n", err);
-            return;
-        }
- 
-        if (minute == 0) {
-            increment_hour();
-        }
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
- 
+
     /**
-     * Increment the hour counter.
+     * Blink the heartbeat led.
      */
-    void increment_hour(void)
+    void blink_led(void)
     {
-        uint8_t hour = 0;
-        ble_error_t err = _hour_char.get(*_server, hour);
-        if (err) {
-            printf("read of the hour value returned error %u\r\n", err);
-            return;
-        }
- 
-        hour = (hour + 1) % 24;
- 
-        err = _hour_char.set(*_server, hour);
-        if (err) {
-            printf("write of the hour value returned error %u\r\n", err);
-            return;
-        }
+        _heartbeat_led = !_heartbeat_led;
     }
  
 private:
@@ -374,27 +225,29 @@ private:
         uint8_t _value;
     };
  
-    ReadNotifyCharacteristic<uint8_t> _hour_char;
-    ReadNotifyCharacteristic<uint8_t> _minute_char;
-    ReadNotifyCharacteristic<uint8_t> _second_char;
+    ReadNotifyCharacteristic<uint8_t> _angle_char;
  
     // list of the characteristics of the clock service
-    GattCharacteristic* _clock_characteristics[3];
+    GattCharacteristic* _hid_characteristics[3];
  
     // demo service
-    GattService _clock_service;
+    GattService _hid_service;
  
     GattServer* _server;
     events::EventQueue *_event_queue;
+
+    AnalogIn _angle_sensor;
+
+    DigitalOut _heartbeat_led;
 };
  
 int main() {
     BLE &ble_interface = BLE::Instance();
     events::EventQueue event_queue;
-    ClockService demo_service;
+    HidService hid_service;
     BLEProcess ble_process(event_queue, ble_interface);
  
-    ble_process.on_init(callback(&demo_service, &ClockService::start));
+    ble_process.on_init(callback(&hid_service, &HidService::start));
  
     // bind the event queue to the ble interface, initialize the interface
     // and start advertising
